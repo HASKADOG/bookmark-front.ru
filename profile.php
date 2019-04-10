db59759ee0c143afa40bc8a74e497ce9bcefcfae
HASKADOG <cplasplas16@gmail.com>
2019-04-10 21:11:05 +0300

    added profile page

---
+++ profile.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Дмитрий Сорокопудов</title>
+	<link rel="stylesheet" href="css/bootstrap.min.css">
+	<link rel="stylesheet" href="css/fontawesome.min.css">
+	<link rel="stylesheet" href="css/profile.css">
+</head>
+<body>
+	<header>
+		<div class="container h-100">
+			<div class="row h-100 align-items-center">
+				<div class="col-3">
+					<div class="prof-red">Профиль пользователя</div>
+				</div>
+			</div>
+		</div>
+	</header>
+
+	<section class="profile p-5">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-auto">
+					<div class="avatar-profile">
+						<img width="200px" src="img/profile_img/1547671809_2_Buddy.jpeg" alt="">
+					</div>
+				</div>
+				<div class="col">
+					<div class="profile-info">
+						<div class="profile-info__name">Имя: Дмитрий Сорокопудов</div>
+						<div class="profile-info__login">Логин: cplasplas</div>
+						<div class="profile-info__email">E-mail: cplasplas16@gmail.com</div>
+						<div class="profile-info__status">Должность: Ученик 10А</div>
+						<div class="profile-info__about-user">Обо мне: Я люблю кошек, собак. Ненавижу химию и биологию. Обожаю программирование, изучаю ардуино.</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+
+	<section class="users-book">
+		<div class="container">
+			<div class="row">
+				<div class="col-auto">
+					<div class="users-book-card d-flex flex-row">
+						<div class="users-book-card__img">
+							<img src="img/books_img/book.jpg" alt="">
+						</div>
+						<div class="users-book-card__about-wrapper d-flex flex-column">
+							<div class="users-book-card__name">Зулейха Окрывает глаза</div>
+							<div class="users-book-card__about-book">О книге</div>
+						</div>
+		
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+</body>
+</html>
