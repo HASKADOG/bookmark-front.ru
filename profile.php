924ad6895cc9da440d6da14a1299c2666dc2a4b0
HASKADOG <cplasplas16@gmail.com>
2019-04-10 22:44:56 +0300

    profile finished

--- profile.php
+++ profile.php
@@ -6,6 +6,7 @@
 	<link rel="stylesheet" href="css/bootstrap.min.css">
 	<link rel="stylesheet" href="css/fontawesome.min.css">
 	<link rel="stylesheet" href="css/profile.css">
+	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
 </head>
 <body>
 	<header>
@@ -17,7 +18,7 @@
 			</div>
 		</div>
 	</header>
-
+	
 	<section class="profile p-5">
 		<div class="container">
 			<div class="row">
@@ -33,12 +34,21 @@
 						<div class="profile-info__email">E-mail: cplasplas16@gmail.com</div>
 						<div class="profile-info__status">Должность: Ученик 10А</div>
 						<div class="profile-info__about-user">Обо мне: Я люблю кошек, собак. Ненавижу химию и биологию. Обожаю программирование, изучаю ардуино.</div>
+						<div class="profile-info__redact-profile">
+							<form action="">
+								<button type="submit"><i class="fas fa-pencil-alt"></i> Редактировать профиль</button>
+							</form>
+						</div>
 					</div>
 				</div>
 			</div>
 		</div>
 	</section>
-
+	<div class="container">
+		<div class="row">
+			<div class="h1 pb-4 col-auto">Ваши книги: </div>
+		</div>
+	</div>
 	<section class="users-book">
 		<div class="container">
 			<div class="row">
@@ -47,13 +57,27 @@
 						<div class="users-book-card__img">
 							<img src="img/books_img/book.jpg" alt="">
 						</div>
-						<div class="users-book-card__about-wrapper d-flex flex-column">
+						<div class="users-book-card__about-wrapper justify-content-between d-flex flex-column">
 							<div class="users-book-card__name">Зулейха Окрывает глаза</div>
-							<div class="users-book-card__about-book">О книге</div>
+							<div class="users-book-card__about-book"><form action=""><button>О книге</button></form></div>
 						</div>
+						<div class="users-book-card__func-wrapper justify-content-around d-flex flex-column align-items-center">
+							<div class="users-book-card__delete">
+								<form action="">
+									<button type="submit"><i class="fas fa-trash-alt"></i></button>
+								</form>
+							</div>
+							<div class="users-book-card__redact">
+								<form action="">
+									<button type="submit"><i class="fas fa-pencil-alt"></i></button>
+								</form>
+							</div>
+						</div>
+						
 		
 					</div>
-				</div>
+				</div> <div class="w-100"></div>
+
 			</div>
 		</div>
 	</section>
